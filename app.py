8d8542441676b6b0c4c6a3397ae006316fc96b1b
Jared Jorgenson <38658539+jared-jorgenson@users.noreply.github.com>
2021-06-06 22:23:53 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from character import *
 pygame.init()
 
 screen_width = 800
-screen_height = 640
+screen_height = 672
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Classic Bomberman")
@@ -17,6 +17,12 @@ def redrawGameWindow():
     screen.blit(bg, (0,0))
     player1.draw(screen)
     player2.draw(screen)
+
+    for bomb in bombs1:
+    	bomb.draw(screen)
+
+    for bomb in bombs2:
+    	bomb.draw(screen)
     
     pygame.display.update()
 
@@ -25,7 +31,13 @@ def redrawGameWindow():
 #####################
 player1 = player(60,60,32,32,5)
 player2 = player(710,550,32,32,5)
-
+#bomb1 = bomb(player1.x, player1.y, 32, 32)
+bombs1 = []
+bombs2 = []
+is_placed1 = False
+is_placed2 = False
+bomb_count1 = 0
+bomb_count2 = 0
 #############
 # main loop #
 #############
@@ -35,7 +47,23 @@ while run:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run = False
+            run = False      
+
+    for weapon in bombs1:
+    	if bomb_count1 > 30:
+    		bombs1.pop(bombs1.index(weapon))
+    		is_placed1 == False
+    		bomb_count1 = 0
+    	else:
+    		bomb_count1 += 1
+
+    for weapon in bombs2:
+    	if bomb_count2 > 30:
+    		bombs2.pop(bombs2.index(weapon))
+    		is_placed2 == False
+    		bomb_count2 = 0
+    	else:
+    		bomb_count2 += 1
 
     keys = pygame.key.get_pressed()
     
@@ -64,6 +92,21 @@ while run:
 
     if keys[pygame.K_DOWN]:
         player2.y += player2.vel
+
+    # bomb mechanics
+    # player1
+    if keys[pygame.K_SPACE]:
+    	if len(bombs1) < 1:
+    		bombs1.append(bomb((player1.x - player1.x % 32),
+    				(player1.y - player1.y % 32),32,32))
+    		is_placed1 == True
+    # player2
+    if keys[pygame.K_SLASH]:
+    	if len(bombs2) < 1:
+    		bombs2.append(bomb((player2.x - player2.x % 32),
+    		(player2.y - player2.y % 32),32,32))
+    		is_placed2 == True
+    	
     
     redrawGameWindow()
     
--- character.py
+++ character.py
@@ -10,4 +10,19 @@ class player(object):
 		self.vel = vel
 
 	def draw(self, screen):
-		pygame.draw.rect(screen, (255,0,0), (self.x, self.y, self.width, self.height))   
+		pygame.draw.rect(screen, (255,0,0), (
+			self.x, self.y, self.width, self.height)
+		)   
+
+class bomb(object):
+	def __init__(self,x,y,width,height):
+		self.x = x
+		self.y = y
+		self.width = width
+		self.height = height
+
+	def draw(self, screen):
+		pygame.draw.rect(screen, (0,0,255), (
+			self.x, self.y, self.width, self.height)
+		)
+
