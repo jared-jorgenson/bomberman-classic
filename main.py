c679af2d9ff5d09a624fb3d3d488b2e8ca5c0831
Michael O'Shea <85414687+Michael-OShea@users.noreply.github.com>
2021-06-27 00:22:07 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import pygame
 from classes import *
 
 
-SCREEN_WIDTH = 480
-SCREEN_HEIGHT = 352
+screen_width = 480
+screen_height = 352
 
 pygame.init()
 
-screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+screen = pygame.display.set_mode([screen_width, screen_height])
 pygame.display.set_caption("Classic Bomberman")
 background_file = "Images/2player_bomberman_map.png"
 bg = pygame.image.load(background_file)
@@ -68,141 +68,145 @@ def redrawGameWindow():
     for bomb in bombs2:
         bomb.draw(screen)
 
-clock = pygame.time.Clock()
+def main():
+    clock = pygame.time.Clock()
 
-run = True
-#Main Loop
-while run:
+    run = True
+    #Main Loop
+    while run:
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:
-                player1.changespeed(-3, 0)
-                player1.left = True
-                player1.front = False
-                player1.back = False
-                player1.right = False
-            elif event.key == pygame.K_d:
-                player1.changespeed(3, 0)
-                player1.left = False
-                player1.front = False
-                player1.back = False
-                player1.right = True
-            elif event.key == pygame.K_w:
-                player1.changespeed(0, -3)
-                player1.left = False
-                player1.front = False
-                player1.back = True
-                player1.right = False
-            elif event.key == pygame.K_s:
-                player1.changespeed(0, 3)
-                player1.left = False
-                player1.front = True
-                player1.back = False
-                player1.right = False
-            elif event.key == pygame.K_LEFT:
-                player2.changespeed(-3, 0)
-                player2.left = True
-                player2.front = False
-                player2.back = False
-                player2.right = False
-            elif event.key == pygame.K_RIGHT:
-                player2.changespeed(3, 0)
-                player2.left = False
-                player2.front = False
-                player2.back = False
-                player2.right = True
-            elif event.key == pygame.K_UP:
-                player2.changespeed(0, -3)
-                player2.left = False
-                player2.front = False
-                player2.back = True
-                player2.right = False
-            elif event.key == pygame.K_DOWN:
-                player2.changespeed(0, 3)
-                player2.left = False
-                player2.front = True
-                player2.back = False
-                player2.right = False
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_a:
+                    player1.changespeed(-3, 0)
+                    player1.left = True
+                    player1.front = False
+                    player1.back = False
+                    player1.right = False
+                elif event.key == pygame.K_d:
+                    player1.changespeed(3, 0)
+                    player1.left = False
+                    player1.front = False
+                    player1.back = False
+                    player1.right = True
+                elif event.key == pygame.K_w:
+                    player1.changespeed(0, -3)
+                    player1.left = False
+                    player1.front = False
+                    player1.back = True
+                    player1.right = False
+                elif event.key == pygame.K_s:
+                    player1.changespeed(0, 3)
+                    player1.left = False
+                    player1.front = True
+                    player1.back = False
+                    player1.right = False
+                elif event.key == pygame.K_LEFT:
+                    player2.changespeed(-3, 0)
+                    player2.left = True
+                    player2.front = False
+                    player2.back = False
+                    player2.right = False
+                elif event.key == pygame.K_RIGHT:
+                    player2.changespeed(3, 0)
+                    player2.left = False
+                    player2.front = False
+                    player2.back = False
+                    player2.right = True
+                elif event.key == pygame.K_UP:
+                    player2.changespeed(0, -3)
+                    player2.left = False
+                    player2.front = False
+                    player2.back = True
+                    player2.right = False
+                elif event.key == pygame.K_DOWN:
+                    player2.changespeed(0, 3)
+                    player2.left = False
+                    player2.front = True
+                    player2.back = False
+                    player2.right = False
 
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_a:
-                player1.changespeed(3, 0)
-            elif event.key == pygame.K_d:
-                player1.changespeed(-3, 0)
-            elif event.key == pygame.K_w:
-                player1.changespeed(0, 3)
-            elif event.key == pygame.K_s:
-                player1.changespeed(0, -3)
-            elif event.key == pygame.K_LEFT:
-                player2.changespeed(3, 0)
-            elif event.key == pygame.K_RIGHT:
-                player2.changespeed(-3, 0)
-            elif event.key == pygame.K_UP:
-                player2.changespeed(0, 3)
-            elif event.key == pygame.K_DOWN:
-                player2.changespeed(0, -3)
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_a:
+                    player1.changespeed(3, 0)
+                elif event.key == pygame.K_d:
+                    player1.changespeed(-3, 0)
+                elif event.key == pygame.K_w:
+                    player1.changespeed(0, 3)
+                elif event.key == pygame.K_s:
+                    player1.changespeed(0, -3)
+                elif event.key == pygame.K_LEFT:
+                    player2.changespeed(3, 0)
+                elif event.key == pygame.K_RIGHT:
+                    player2.changespeed(-3, 0)
+                elif event.key == pygame.K_UP:
+                    player2.changespeed(0, 3)
+                elif event.key == pygame.K_DOWN:
+                    player2.changespeed(0, -3)
 
-    for weapon in bombs1:
-        if weapon.bomb_count > 120:
-            bombs1.pop(bombs1.index(weapon))
-            weapon.bomb_count = 0
-        else:
-            weapon.bomb_count += 1
-
-    for weapon in bombs2:
-        if weapon.bomb_count > 120:
-            bombs2.pop(bombs2.index(weapon))
-            weapon.bomb_count = 0
-        else:
-            weapon.bomb_count += 1
-
-    keys = pygame.key.get_pressed()
-    # bomb mechanics
-    # player1
-    if keys[pygame.K_SPACE]:
-        if len(bombs1) < 1:
-            # player is left and up
-            if player1.rect.x % 32 < 16 and player1.rect.y % 32 < 16:
-                bombs1.append(bomb((player1.rect.x - player1.rect.x % 32+3),
-                                (player1.rect.y - player1.rect.y % 32+1), 32, 32, 0))
-            # player is left and down
-            elif player1.rect.x % 32 < 16 and player1.rect.y % 32 >= 16:
-                bombs1.append(bomb((player1.rect.x - player1.rect.x % 32+3),
-                                (player1.rect.y - player1.rect.y % 32+33), 32, 32, 0))
-            # player is right and up
-            elif player1.rect.x % 32 >= 16 and player1.rect.y % 32 < 16:
-                bombs1.append(bomb((player1.rect.x - player1.rect.x % 32+35),
-                                (player1.rect.y - player1.rect.y % 32+1), 32, 32, 0))
-            # player is right and down
+        for weapon in bombs1:
+            if weapon.bomb_count > 120:
+                bombs1.pop(bombs1.index(weapon))
+                weapon.bomb_count = 0
             else:
-                bombs1.append(bomb((player1.rect.x - player1.rect.x % 32+35),
-                                (player1.rect.y - player1.rect.y % 32+33), 32, 32, 0))
-    # player2
-    if keys[pygame.K_SLASH]:
-        if len(bombs2) < 1:
-            # player is left and up
-            if player2.rect.x % 32 < 16 and player2.rect.y % 32 < 16:
-                bombs2.append(bomb((player2.rect.x - player2.rect.x % 32+3),
-                                (player2.rect.y - player2.rect.y % 32+1), 32, 32, 0))
-            # player is left and down
-            elif player2.rect.x % 32 < 16 and player2.rect.y % 32 >= 16:
-                bombs2.append(bomb((player2.rect.x - player2.rect.x % 32+3),
-                                (player2.rect.y - player2.rect.y % 32+33), 32, 32, 0))
-            # player is right and up
-            elif player2.rect.x % 32 >= 16 and player2.rect.y % 32 < 16:
-                bombs2.append(bomb((player2.rect.x - player2.rect.x % 32+35),
-                                (player2.rect.y - player2.rect.y % 32+1), 32, 32, 0))
-            # player is right and down
+                weapon.bomb_count += 1
+
+        for weapon in bombs2:
+            if weapon.bomb_count > 120:
+                bombs2.pop(bombs2.index(weapon))
+                weapon.bomb_count = 0
             else:
-                bombs2.append(bomb((player2.rect.x - player2.rect.x % 32+35),
-                                (player2.rect.y - player2.rect.y % 32+33), 32, 32, 0))
+                weapon.bomb_count += 1
 
-    redrawGameWindow()
-    pygame.display.flip()
+        keys = pygame.key.get_pressed()
+        # bomb mechanics
+        # player1
+        if keys[pygame.K_SPACE]:
+            if len(bombs1) < 1:
+                # player is left and up
+                if player1.rect.x % 32 < 16 and player1.rect.y % 32 < 16:
+                    bombs1.append(bomb((player1.rect.x - player1.rect.x % 32+3),
+                                    (player1.rect.y - player1.rect.y % 32+1), 32, 32, 0))
+                # player is left and down
+                elif player1.rect.x % 32 < 16 and player1.rect.y % 32 >= 16:
+                    bombs1.append(bomb((player1.rect.x - player1.rect.x % 32+3),
+                                    (player1.rect.y - player1.rect.y % 32+33), 32, 32, 0))
+                # player is right and up
+                elif player1.rect.x % 32 >= 16 and player1.rect.y % 32 < 16:
+                    bombs1.append(bomb((player1.rect.x - player1.rect.x % 32+35),
+                                    (player1.rect.y - player1.rect.y % 32+1), 32, 32, 0))
+                # player is right and down
+                else:
+                    bombs1.append(bomb((player1.rect.x - player1.rect.x % 32+35),
+                                    (player1.rect.y - player1.rect.y % 32+33), 32, 32, 0))
+        # player2
+        if keys[pygame.K_SLASH]:
+            if len(bombs2) < 1:
+                # player is left and up
+                if player2.rect.x % 32 < 16 and player2.rect.y % 32 < 16:
+                    bombs2.append(bomb((player2.rect.x - player2.rect.x % 32+3),
+                                    (player2.rect.y - player2.rect.y % 32+1), 32, 32, 0))
+                # player is left and down
+                elif player2.rect.x % 32 < 16 and player2.rect.y % 32 >= 16:
+                    bombs2.append(bomb((player2.rect.x - player2.rect.x % 32+3),
+                                    (player2.rect.y - player2.rect.y % 32+33), 32, 32, 0))
+                # player is right and up
+                elif player2.rect.x % 32 >= 16 and player2.rect.y % 32 < 16:
+                    bombs2.append(bomb((player2.rect.x - player2.rect.x % 32+35),
+                                    (player2.rect.y - player2.rect.y % 32+1), 32, 32, 0))
+                # player is right and down
+                else:
+                    bombs2.append(bomb((player2.rect.x - player2.rect.x % 32+35),
+                                    (player2.rect.y - player2.rect.y % 32+33), 32, 32, 0))
 
-    clock.tick(60)
+        redrawGameWindow()
+        pygame.display.flip()
 
-pygame.quit()
+        clock.tick(60)
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
